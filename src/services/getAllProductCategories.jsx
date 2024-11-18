9f04dc52cba41bea602a3cf9d1a5a6408af0cfca
muhammaddaffashafwan <muhdaffashafwan@gmail.com>
2024-11-18 17:18:04 +0700

    initial commit

---
+++ src/services/getAllProductCategories.jsx
@@ -0,0 +1,21 @@
+const getAllProductCategories = () => {
+    return [
+      {
+        id:'CRR',
+        slug: 'carrier',
+        name: 'Carrier'
+      },
+      {
+        id:'SHS',
+        slug: 'shoes',
+        name: 'Shoes'
+      },
+      {
+        id:'TNT',
+        slug: 'tent',
+        name: 'Tent'
+      }
+    ]
+}
+
+export default getAllProductCategories
---
+++ src/services/getAllProducts.js
@@ -0,0 +1,325 @@
+export default function getAllProducts() {
+    return [
+      {
+        id: 'CRR-001',
+        slug: 'Carrier Eiger Equator',
+        name: " Tas Carier Eiger Equator - 45L",
+        category: 'Carrier',
+        categorySlug: 'carrier',
+        price: 950_000,
+        stock: 5,
+        imageUrl: '/asset/images/carir-eiger2.jpg',
+        description: `Equator 45 adalah tas carrier berkapasitas 45 liter yang dirancang untuk kegiatan trekking dan hiking selama 1-2 hari. Tas ini memiliki kompartemen utama dengan pemisah/sekat dan kompartemen eksternal, seperti saku samping, saku tutup atas, dan saku hip belt untuk menjaga barang bawaan Anda tetap aman dan terjaga. Tas ini juga dilengkapi dengan teknologi backsystem Aerovent Trek yang membuat sirkulasi udara di punggung Anda lebih optimal. Equator 45 juga memiliki fitur-fitur lain untuk mendukung perannya sebagai tas carrier ideal untuk perjalanan trekking Anda seperti pengikat trekking pole, kompartemen untuk menyimpan water bladder, tali kompresi samping, dan rain cover untuk melindungi tas dari air hujan.
+
+Fitur:
+- Saku depan
+- Kompartemen utama dengan pemisah/sekat
+- Saku tutup atas
+- Lubang untuk selang water bladder (kantung air)
+- Kompartemen untuk menyimpan water bladder (kantung air)
+- 2 saku samping
+- Tali kompresi samping
+- Tali kompresi bawah
+- Pengikat trekking pole
+- Saku hip belt
+- Rain cover untuk melindungi tas dari air hujan
+- Aksen reflektif, agar mudah terlihat dalam keadaan minim cahaya atau malam hari
+- Tali dada
+- Robic adalah material berkualitas tinggi yang berdaya tahan kuat, mampu menahan gesekan dan abrasi, dengan tanpa mengurangi tampilannya yang tetap terlihat awet.
+- Aerovent Trek adalah teknologi backsystem yang efektif meminimalisir keringat berlebih pada punggung dengan menyediakan ruang lebih untuk sirkulasi udara.
+
+.`
+      },
+      {
+        id: 'CRR-002',
+        slug: 'Carrier Consina Tarebbi',
+        name: " Tas Carier Consina Tarebbi - 60L ",
+        category: 'Carrier',
+        categorySlug: 'carrier',
+        price: 874_000,
+        stock: 5,
+        imageUrl: '/asset/images/carir-consina2.jpg',
+        description: `CARRIER TAS GUNUNG KERIL CONSINA TAREBBI ORI 100% NO KW
+IDR. 874rb
+
+Spesifikasi:
+-Bahan Soft Cordura Duratex Polyester Webbing
+-Kapasitas 60 Liter Up To 70 Liter
+-Dobel Frame Alumunium Dibelakang Yg Mudah Dilepas
+-Backsystem Super Nyaman Karena Didukung Busa-Busa Super Tebal Berkualitas Dan Pastinya Tidak Gampang Kempes
+-Tali-Tali Pengatur dan Penyeimbang Yg Mudah Sekali Diatur
+-1 Kantong Utama Dilengkapi Dgn Slot Laptop/Waterbladder Dll, 1 Kantong Ditutup Tas, 2 Kantong Memanjang Dibagian Kanan-Kiri Dgn Slerekan, 2 Kantong Kecil Kanan-Kiri Tas, 2 Saku Di-Sabuk Pinggang Kanan-Kiri
+-Terdapat Saluran H2o Guna Memasang Selang Air Bila Diperlukan
+-Pastilah Nyaman Digendong
+-Sudah Clear Jelas Ok ya!!!
+
+Asli Produk Anak Negeri ; Love Produk Indonesia
+
+.`
+      },
+      {
+        id: 'CRR-003',
+        slug: 'Carrier Arel Cartenz',
+        name: "Tas Carier Arei Cartenz - 60L ",
+        category: 'Carrier',
+        categorySlug: 'carrier',
+        price: 1_374_000,
+        stock: 0,
+        imageUrl: '/asset/images/carir-arai.jpg',
+        description: `Tas gunung Antarestar mungkin dirancang khusus untuk aktivitas hiking dan pendakian dengan fitur-fitur unggul yang cocok untuk medan pegunungan dan perjalanan jarak jauh. Berikut adalah deskripsi yang cocok untuk tas gunung dengan nama ini: 
+
+                        Kapasitas Besar: Tas gunung Antarestar memiliki kapasitas yang cukup besar, biasanya berkisar antara 50 hingga 80 liter, memungkinkan para pendaki membawa peralatan lengkap seperti tenda, kantong tidur, pakaian, dan persediaan makanan.
+
+                        Material Tahan Air dan Tahan Lama: Terbuat dari bahan berkualitas tinggi seperti nylon ripstop atau polyester tahan air yang melindungi barang bawaan dari hujan dan kondisi cuaca ekstrem di pegunungan.
+
+                        Desain Ergonomis: Tas ini dilengkapi dengan punggung berbentuk khusus yang mengikuti lekuk tubuh, tali bahu yang empuk dan bisa diatur, serta sabuk pinggang yang lebar untuk mengurangi beban pada bahu dan mendistribusikannya ke pinggul.
+
+                        Ventilasi dan Sistem Sirkulasi Udara: Menggunakan teknologi sirkulasi udara di bagian punggung untuk mencegah panas berlebih dan keringat berlebih saat mendaki.
+
+.`
+      }, {
+        id: 'CRR-004',
+        slug: 'Carrier Avtech Malabo',
+        name: "Tas Carier Avtech Malabo - 45L ",
+        category: 'Carrier',
+        categorySlug: 'carrier',
+        price: 417_000,
+        stock: 5,
+        imageUrl: '/asset/images/carir-avtech.jpg',
+        description: `Carier Malabo 45L 0360
+*Material :
+-Nylon Cordusa, Nylon Dolgy
+-Ziper SBS
+-Frame alumunium
+- 1 kompertemen utama
+- 1 saku dibagian topi
+- 2 saku dibagian samping
+- 1 saku raincover
+- single frame
+- dilengkapi pluit
+
+.`
+      },
+      {
+        id: 'SHS-005',
+        slug: 'Consina-Mustang',
+        name: "Shoes Consina Mustang ",
+        category: 'Shoes',
+        categorySlug: 'shoes',
+        price: 1_500_000,
+        stock: 5,
+        imageUrl: `/asset/images/shoes-consina.jpg`,
+        description: `Sepatu Gunung Consina Mustang
+Size: 39, 40, 41, 42, 43, 44
+
+Fitur:
+- Kulit suede yang kuat dan bahan oxford bagian atas
+- Desain tinggi memberikan perlindungan ekstra untuk kaki Anda
+- Penutup kaki karet dan struktur pendukung tumit
+- Uneebtex membran tahan air dan bersirkulasi menjaga kaki Anda kering dan nyaman
+- Sistem lace-up yang aman memberi Anda kecocokan dan nyaman yang Anda inginkan
+- EVA mid-sol
+- Komodo, Sol karet alami yang anti slip
+
+Sol peredam tahan air
+- Desain terbaru
+- Mengontrol pijakan dengan baik
+- Menjaga posisi kenyamanan kaki
+- Kontur tapak sepatu yang anti llicin
+- Meredam benturan`
+      },
+      {
+        id: 'SHS-006',
+        slug: 'Eiger-Cayman-Lite',
+        name: "Shoes Eiger Cayman Lite",
+        category: 'Shoes',
+        categorySlug: 'shoes',
+        price: 885_000,
+        stock: 5,
+        imageUrl: `/asset/images/shoes-eiger.jpg`,
+        description: `Brand= Eiger
+Tipe= Cayman Lite Shoes
+Bahan= Microfiber Leather and Poly Mesh
+Warna= Coklat / Hitam / Coklat Tua
+*mohon tanyakan ketersediaan stok dan warna sebelum membeli dan pastikan pilihan anda di varian sudah sesuai
+
+Fitur:
+1. Bagian atas sepatu menggunakan bahan microfiber leather (kulit microfiber adalah kulit sintetis yang memiliki kemiripan dengan strukturkulit asli) dan poly mesh
+2. Konstruksi gusset dengan waterproof membrane untuk mencegah langsung masuknya air
+3. Insole Ortholite
+4. Sol berbahan karet
+Size 46 panjang insole: 30,7 cm
+
+100% ORIGINAL WITH BOX`
+      },
+      {
+        id: 'SHS-007',
+        slug: 'Hi-Tec-Altitude-Alpyna',
+        name: "Shoes HI-TEC Altitude Alpyna",
+        category: 'Shoes',
+        categorySlug: 'shoes',
+        price: 1_699_900,
+        stock: 0,
+        imageUrl: `/asset/images/shoes-hitec.jpg`,
+        description: `Tas gunung Eiger adalah salah satu produk populer dari brand outdoor terkenal asal Indonesia, Eiger Adventure. Berikut adalah deskripsi umum untuk tas gunung Eiger, yang dirancang khusus untuk mendukung kegiatan pendakian dan perjalanan petualangan di alam bebas:
+
+                        Kapasitas dan Varian Ukuran: Tas gunung Eiger hadir dalam berbagai kapasitas, dari yang kecil (20-40 liter) untuk pendakian singkat hingga yang besar (50-75 liter) untuk ekspedisi panjang. Kapasitas besar ini memungkinkan pendaki membawa perlengkapan lengkap, seperti tenda, kantong tidur, pakaian, dan makanan.
+
+                        Material Kuat dan Tahan Air: Eiger menggunakan material berkualitas tinggi seperti nylon ripstop dan polyester yang tahan lama, tahan air, dan tahan abrasi. Beberapa tas juga dilengkapi dengan rain cover untuk perlindungan ekstra dari hujan dan cuaca ekstrem.
+
+                        Sistem Backpanel dan Ventilasi: Tas Eiger memiliki sistem backpanel ergonomis dengan bantalan busa dan sirkulasi udara yang baik, sehingga nyaman digunakan dalam perjalanan panjang dan mencegah panas berlebih pada punggung..`
+      },
+      {
+        id: 'SHS-008',
+        slug: 'Karimor-Original-Boot',
+        name: "Shoes Karimor Original Boot",
+        category: 'Shoes',
+        categorySlug: 'shoes',
+        price: 747_000,
+        stock: 5,
+        imageUrl: `/asset/images/sepatu-eiger.webp`,
+        description: `SepatuGunung|Keren|Hits|Promo|Diskon|Baru|Top|Best|Hot|Laris|Update
+
+SEPATU GUNUNG WATERPROOF KARRIMOR SUMMIT ORIGINAL (GAK ORI DUIT KEMBALI)
+BARANG BARU TANPA BOX
+
+UNTUK KETERSEDIAAN WARNA DAN UKURAN, SILAHKAN CHAT ADMIN TERLEBIH DAHULU
+
+PANDUAN UKURAN :
+39 : 24.5CM
+40 : 25.2CM
+41 : 26.0CM
+42 : 26.8CM
+43 : 27.5CM
+44 : 28.2CM
+45 : 29.0CM`
+      },
+      {
+        id: 'TNT-009',
+        slug: 'Eiger-X-Casuarina',
+        name: "Tent Eiger X-Casuarina ",
+        category: 'Tent',
+        categorySlug: 'tent',
+        price: 1_919_000,
+        stock: 3,
+        imageUrl: `/asset/images/tenda-eiger.jpg`,
+        description: `Memilki desain yang cukup luas dan nyaman, tenda Casuarina 6P siap menemani petualangan berkemah Anda bersama teman dan keluarga. Ideal untuk kapasitas enam orang, tenda Casuarina 6P ini hadir dengan desain satu pintu masuk dan teras tenda memanjang yang menawarkan kemudahan untuk menyimpan peralatan Anda. Terbuat dari material waterproof polyester dengan konstruksi double-layer serta dilengkapi triangular rag, tenda Casuarina 6P ini menawarkan perlindungan yang tahan terhadap kondisi cuaca hujan ataupun berangin. Selain mudah dipasang dan multifungsi, tenda Casuarina 6P juga dilenkapi dengan breathable panel mesh yang memberikan ventilasi sekaligus menawarkan akses untuk menikmati pemandangan alam yang menakjubkan. Ketika selesai dipakai, Anda dapat menyimpannya kembali di carry bag.
+
+Dimension: 270 x (80+270) x 155cm
+Dimension Packaging: 60 x 15 x 15cm
+
+Fly: 190T Polyester PU 2000mm
+Mesh: B3
+Floor: PE
+Pole: Fiberglass 9.5mm
+
+Fitur:
+Tenda double layer dengan material waterproof polyester
+Dilengkapi akses satu pintu masuk dengan ritsleting 2 arah dan teras tenda
+Dirancang dengan ventilasi berbentuk segitiga dan panel mesh untuk meningkatkan sirkulasi udara
+Dilengkapi 6 saku pada bagian dalam tenda untuk menyimpan barang esensial secara rapi
+Pengait di bagian dalam tenda untuk mengaitkan lentera lampu atau senter
+Guyline reflektif membantu lebih mudah terlihat di malam hari atau saat keadaan minim cahaya
+Rag berbentuk segitiga pada bagian luar tenda berfungsi untuk memperkuat tenda tahan terhadap kondisi berangin
+Dilengkapi pasak tambahan
+Dilengkapi carry bag untuk memudahkan penyimpanan tenda saat di bawa berpergian
+
+Penting!
+Diwajibkan pembeli merekam video saat pembukaan paket. Tanpa adanya bukti video, akan sangat sulit untuk kami memproses keluhan anda
+
+Catatan : Harap Konfirmasi stok terlebih dahulu pada kami lewat chat atau diskusi. karena stok dapat berubah sewaktu-waktu.`
+      },
+      {
+        id: 'TNT-010',
+        slug: 'Consina-Kathmandu',
+        name: "Tent Consina Kathmandu",
+        category: 'Tent',
+        categorySlug: 'tent',
+        price: 1_879_000,
+        stock: 3,
+        imageUrl: `/asset/images/tenda-consina.jpg`,
+        description: `Tenda consina Khatmandu 4
+
+Outer : 210T polyester PU COATED
+Inner : PU coated nylon with mesh
+Floor: PU coated nylon
+Isi kemasan 48 x 15 x 13 cm:
+- Frame aluminium 2 set
+- Pasak 12 buah
+- Tali pengikat 4 pcs
+- Tenda bagian dalam
+- Cover tenda
+
+Fitur:
+- Dimensi 208 x 180 x 130 cm (P x L x T)
+- Kapasitas maks. 4 orang
+- Dua arah keluar-masuk dan teras
+- Dua kantong dalam tenda
+- Cover bahan tahan air
+- Anti badai
+- Tas kemasan
+
+
+Selama iklan masih ada barang ready ya.
+Pengiriman merupakan Tanggung jawab pihak Kurir Expedisi.
+
+NO REFUND !!!
+MEMBELI = SETUJU
+
+SERTAKAN VIDEO UNBOXING JIKA ADA KENDALA, KAMI
+TIDAK MENERIMA KOMPLAIN JIKA TIDAK ADA VIDEO
+UNBOXING !!!
+
+Selamat Berbelanja.
+Terimakasih.
+`
+      },
+      {
+        id: 'TNT-011',
+        slug: 'Arei-Original-Cladonia',
+        name: "Tent Arei Original Cladonia",
+        category: 'Tent',
+        categorySlug: 'tent',
+        price: 503_000,
+        stock: 3,
+        imageUrl: `/asset/images/tenda-arai.jpg`,
+        description: `Tas gunung Consina adalah salah satu pilihan favorit di Indonesia untuk pendaki dan pecinta alam. Brand ini dikenal dengan produk-produk yang tangguh, fungsional, dan terjangkau. Berikut adalah deskripsi umum untuk tas gunung Consina:
+
+                        Kapasitas dan Varian Ukuran: Tas gunung Consina tersedia dalam berbagai kapasitas, mulai dari ukuran kecil (20-40 liter) untuk kegiatan sehari-hari atau perjalanan singkat, hingga ukuran besar (50-100 liter) untuk pendakian panjang dan ekspedisi. Kapasitas yang beragam ini memungkinkan pengguna untuk memilih tas sesuai kebutuhan petualangan mereka.
+
+                        Material Tahan Lama dan Tahan Air: Consina menggunakan bahan berkualitas tinggi seperti polyester dan nylon ripstop yang tahan air dan tahan lama. Bahan ini mampu melindungi isi tas dari hujan dan cuaca ekstrem, serta memiliki ketahanan terhadap goresan dan abrasi. Beberapa model dilengkapi dengan rain cover untuk perlindungan tambahan dari hujan..`
+      },
+      {
+        id: 'TNT-012',
+        slug: 'New-Quecha-Arpenaz-Family',
+        name: "Tent New Quechua Arpenaz Family",
+        category: 'Tent',
+        categorySlug: 'tent',
+        price: 7_168_000,
+        stock: 0,
+        imageUrl: `/asset/images/tenda-decathon.jpg`,
+        description: `Tenda Camping Family 4.1 / 4P 
+
+USER BENEFITS :
+- Kapasitas : Ruang tamu yang luas 5.9 m² dengan alas lantai, kamar tidur 210 x 240 cm.
+- Mudah dibongkar/pasang : Tiang sederhana: tiang dengan kode .
+- Mengurangi panas : Flysheet berlapis di atas ruang tamu untuk ventilasi dan mengurangi kondensasi.
+- Kegelapan
+- Daya tahan : Menahan angin 50km/jam (Daya 6): diuji di terowongan angin pada bidang berputar
+- Tahan air : Diuji shower dengan 200 mm air / jam / m² (hujan tropis) dan uji lapangan.
+
+[EN] Composition :
+Kain luar : 100.0% Poliester (PES) Tas jinjing : 100.0% Poliester (PES) Ruang dalam : 100.0% Poliester (PES) Tiang : 100.0% Kaca - Serat (FG) Keset lantai ‎ : 100.0% Polyethylene (PE)
+
+[EN] Care advice :
+1.) PEMBERSIHAN: bersihkan tenda dengan air sabun bila perlu, kemudian bilas (dengan selang bila memungkinkan). Keringkan sepenuhnya sebelum disimpan. 2.) NODA GETAH: sangat sulit dibersihkan: begitu kering, korek noda dengan lembut. Menggosokkan es batu pada sisi bawah membuatnya jadi berkerak, sehingga noda lebih mudah dihilangkan. 3.) PENYIMPANAN: setelah digunakan atau dibersihkan, jangan simpan tenda Anda hingga benar-benar kering untuk mencegah bau dan berjamur.
+
+[EN] Storage advice :
+Penyimpanan - setelah digunakan atau dibersihkan, jangan simpan tenda Anda hingga benar-benar kering untuk mencegah bau dan berjamur.
+
+[EN] Use restriction :
+Ruang tidur dengan maksimal 60 cm per orang.`
+      },
+  
+    ]
+  }
